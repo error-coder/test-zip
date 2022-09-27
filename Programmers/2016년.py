6f4e62e2ee7ced7eb309f3b9bff46a46ca357da7
error-coder <113674396+error-coder@users.noreply.github.com>
2022-09-27 16:14:29 +0900

    Add files via upload

---
+++ Programmers/2016년.py
@@ -0,0 +1,8 @@
+def solution(a,b):
+
+    day = ['FRI','SAT','SUN','MON','TUE','WED','THU']
+    LastDay = [31,29,31,30,31,30,31,31,30,31,30,31]
+
+    return day[(sum(LastDay[:a-1]) + b - 1) % 7]
+
+print(solution(5,24))
