7f0fb2cc4081d6f99f795bc458a3a970efcab701
error-coder <113674396+error-coder@users.noreply.github.com>
2022-10-05 18:23:28 +0900

    Update 크레인 인형뽑기.py

--- Programmers/크레인 인형뽑기.py
+++ Programmers/크레인 인형뽑기.py
@@ -13,22 +13,27 @@
 def solution(board, moves):
     stack = []
     count = 0
-
+    # 크레인 움직임 출력
     for turn in moves:
         now = turn - 1
+        # 해당 칸에서 뽑기가 끝나면(while 루프 끝나면) 알아서 x가 메모리 해제 -> 다시 for 루프에 들어와서 x=0으로 초기화
         x = 0
-
+        # while 문이 x가 보드의 길이를 다 돌때까지 반복
         while (x != len(board)):
+            # 칸에 구슬이 있을 때
             if board[x][now] != 0:
+                # 스택 길이가 0이 아니고 마지막 원소가 칸에 있는 인형의 숫자와 같을 경우
                 if len(stack) != 0 and stack[-1] == board[x][now]:
                     board[x][now] = 0
+                    # 구슬이 겹치기 때문에 삭제됨
                     stack.pop()
+                    # 인형의 개수가 2개가 삭제되어 2개를 더해줌
                     count += 2
                 else:
                     stack.append(board[x][now])
                     board[x][now] = 0
                 break
-
+            # 인형을 찾지 못했으므로 다음 열로 이동
             x += 1
         
 
